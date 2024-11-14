b2a89e086e4f53443c6e54741c0f4a908c7343aa
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-11-14 11:37:04 -0300

    feat(modal): desenvolve HTML e CSS

--- libs/storybook-host/src/lib/Molecules/modal/modal.component.stories.ts
+++ libs/storybook-host/src/lib/Molecules/modal/modal.component.stories.ts
@@ -1,8 +1,16 @@
-import type { Meta, StoryObj } from '@storybook/angular';
+import { argsToTemplate, type Meta, type StoryObj } from '@storybook/angular';
 import { ModalComponent } from '@alfabit-alura/modal';
 
 const meta: Meta<ModalComponent> = {
   component: ModalComponent,
+  render: (args) => ({
+    props: args,
+    template: `
+      <ab-modal ${argsToTemplate(args)}>
+        Conteúdo qualquer
+      </ab-modal>
+    `,
+  }),
 };
 
 export default meta;
@@ -11,6 +19,7 @@ type Story = StoryObj<ModalComponent>;
 
 export const Open: Story = {
   args: {
+    modalTitle: 'Heading',
     isOpen: true,
   },
 };
--- libs/ui/modal/src/lib/modal/modal.component.ts
+++ libs/ui/modal/src/lib/modal/modal.component.ts
@@ -6,16 +6,18 @@ import {
   ViewChild,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TypographyComponent } from '@alfabit-alura/typography';
 
 @Component({
   selector: 'ab-modal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TypographyComponent],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.css',
 })
 export class ModalComponent implements AfterViewChecked {
   @Input() isOpen = false;
+  @Input() modalTitle = '';
 
   @ViewChild('modal') modal!: ElementRef<HTMLDialogElement>;
 

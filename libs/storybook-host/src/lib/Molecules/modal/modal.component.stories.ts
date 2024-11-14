94ede340d4f850733bb093256978416f6fd51b39
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-11-14 11:51:04 -0300

    feat(storybook-host): utiliza typography e button no story do modal

--- libs/storybook-host/src/lib/Molecules/modal/modal.component.stories.ts
+++ libs/storybook-host/src/lib/Molecules/modal/modal.component.stories.ts
@@ -1,13 +1,34 @@
-import { argsToTemplate, type Meta, type StoryObj } from '@storybook/angular';
+import {
+  argsToTemplate,
+  moduleMetadata,
+  type Meta,
+  type StoryObj,
+} from '@storybook/angular';
 import { ModalComponent } from '@alfabit-alura/modal';
+import { ButtonComponent } from '@alfabit-alura/button';
+import { TypographyComponent } from '@alfabit-alura/typography';
 
 const meta: Meta<ModalComponent> = {
   component: ModalComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [ButtonComponent, TypographyComponent],
+    }),
+  ],
   render: (args) => ({
     props: args,
     template: `
       <ab-modal ${argsToTemplate(args)}>
-        Conteúdo qualquer
+        <p>
+          <ab-typography>
+            Today, every company I talk to wants to implement a design system from
+            scratch. Unfortunately, the details of a design system are not
+            explored, so often they are not used to the maximum to create a useful
+            user experience.
+          </ab-typography>
+        </p>
+
+        <ab-button>Primary button</ab-button>
       </ab-modal>
     `,
   }),
